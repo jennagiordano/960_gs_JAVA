8042a512ee5894ceec34a0c90b952a4317c9f567
jennagiordano <31827859+jennagiordano@users.noreply.github.com>
2017-11-16 12:11:25 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,70 @@
+function changeForest () {
+
+	//get an element with id hello
+	var newColor = document.getElementById("hello");
+
+	//get an element with id rColor
+	var newColor2 = document.getElementById("rColor");
+
+	// access style and change backgroundColor property 
+	newColor.style.backgroundColor = 'green';
+
+	//change header font color
+	rColor.style.color = 'green';
+
+
+}
+
+
+function changeBeach () {
+
+	//get an element with id hello
+	var newColor = document.getElementById("hello");
+
+	//get an element with id bColor
+	var newColor2 = document.getElementById("bColor");
+
+	// access style and change backgroundColor property 
+	newColor.style.backgroundColor = 'lightpink';
+
+	//change header font color
+	bColor.style.color = 'lightpink';
+
+}
+
+
+function changeDesert () {
+
+	//get an element with id hello
+	var newColor = document.getElementById("hello");
+
+	//get an element with id dColor
+	var newColor2 = document.getElementById("dColor");
+
+	//change backgroundColor property 
+	newColor.style.backgroundColor = 'sienna';
+
+	//change header font color
+	dColor.style.color = 'sienna';
+
+}
+
+function changeMountains () {
+
+	//get an element with id hello
+	var newColor = document.getElementById("hello");
+
+
+	//get an element with id mColor
+	var newColor2 = document.getElementById("mColor");
+
+
+	//change backgroundColor property 
+	newColor.style.backgroundColor = 'lightblue';
+
+	//change header font color
+	mColor.style.color = 'lightblue';
+
+}
+
+
